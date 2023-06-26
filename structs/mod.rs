a74a73f6d93c4d00f316f291aa09995f4a1fa633
JoacoSlime <JoacoSlime@proton.me>
2023-06-26 19:32:38 -0300

    Eliminando structs deprecados.

--- structs/mod.rs
+++
@@ -1,5 +0,0 @@
-mod socio;
-mod categoria;
-mod deporte;
-mod recibo;
-mod pago;
--- structs/recibo.rs
+++
@@ -1,8 +0,0 @@
-use super::categoria::Categoria;
-
-pub struct Recibo {
-    nombre: String,
-    dni: u32,
-    monto: u128,
-    categoria: Categoria,
-}
