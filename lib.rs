249ffd30396abdbe194358edd0d0c0b7c2c78382
JoacoSlime <JoacoSlime@proton.me>
2023-07-10 15:56:13 -0300

    Agregados tests de GestorDeCobros

--- lib.rs
+++ lib.rs
@@ -196,6 +196,9 @@ mod gestor_de_cobros {
 
     #[cfg(test)]
     mod gestor_de_cobros_tests {
+        use club_sem_rust::Socio;
+
+        use super::GestorDeCobros;
         use super::Recaudacion;
 
         #[ink::test]
@@ -213,7 +216,95 @@ mod gestor_de_cobros {
             assert_eq!(esperado, resultado, "se esperaba {:?} y se recibió {:?}", esperado, resultado)
         }
 
+        #[ink::test]
+        fn socios_no_morosos_test() { // Debe extenderse
+            let deadline = 864_000_000;
+            let hoy: crate::gestor_de_cobros::Timestamp = 1_690_000_000_000;
+            let precios = Vec::from([5000,4000,2000]);
+            let mut a = Socio::new("Alice".to_string(), 44044044, 1, None, hoy+deadline, precios.clone());
+            let mut c = Socio::new("Carol".to_string(), 46046046, 2, Some(1), hoy+deadline, precios.clone());
+            let mut d = Socio::new("Derek".to_string(), 47047047, 1, None, hoy+deadline, precios.clone());
+            let e = Socio::new("Emily".to_string(), 48048048, 1, None, hoy+deadline, precios.clone());
+            a.realizar_pago(None, 5000, hoy, precios.clone(), deadline);
+            a.realizar_pago(None, 5000, hoy + 100_000, precios.clone(), deadline);
+            a.realizar_pago(Some(15), 4250, hoy + 300_000, precios.clone(), deadline);
+
+            c.realizar_pago(None, 4000, hoy, precios.clone(), deadline);
+            c.realizar_pago(None, 4000, hoy + 100_000, precios.clone(), deadline);
+
+            d.realizar_pago(None, 5000, hoy, precios.clone(), deadline);
+            d.realizar_pago(None, 5000, hoy + 100_000, precios.clone(), deadline);
+            d.realizar_pago(Some(15), 4250, hoy + deadline + 200_000, precios.clone(), deadline);
+
+            let esperado: Vec<Socio> = Vec::from([a,c,d,e]);
+            let gestor = GestorDeCobros::new();
+            let resultado = gestor.socios_no_morosos(1);
+
+            assert_eq!(esperado, resultado, "Se esperaba {:#?} y se obtuvo {:#?}", esperado, resultado);
+        }
+
+        #[ink::test]
+        fn socios_morosos_test() {
+            let hoy: crate::gestor_de_cobros::Timestamp = 1_690_000_000_000;
+            let deadline = 864_000_000;
+
+            let precios = Vec::from([5000, 4000, 2000]);
+            let descuento = Some(15);
+            let mut a = Socio::new("Alice".to_string(), 44044044, 1, None, hoy+deadline, precios.clone());
+            let mut b = Socio::new("Bob".to_string(), 45045045, 2, Some(6), hoy+deadline, precios.clone());
+            let mut c = Socio::new("Carol".to_string(), 46046046, 2, Some(1), hoy+deadline, precios.clone());
+            let mut d = Socio::new("Derek".to_string(), 47047047, 1, None, hoy+deadline, precios.clone());
+            let e = Socio::new("Emily".to_string(), 48048048, 1, None, hoy+deadline, precios.clone());
+            let mut f = Socio::new("Frank".to_string(), 49049049, 3, None, hoy+deadline, precios.clone());
+            let g = Socio::new("Gary".to_string(), 50050050, 3, None, hoy+deadline, precios.clone());
+            
+            a.realizar_pago(None, 5000, hoy, precios.clone(), deadline);
+            a.realizar_pago(None, 5000, hoy + 100_000, precios.clone(), deadline);
+            a.realizar_pago(descuento, 4250, hoy + 300_000, precios.clone(), deadline);
+
+            b.realizar_pago(None, 4000, hoy, precios.clone(), deadline);
+            b.realizar_pago(None, 4000, hoy + deadline + 100_000, precios.clone(), deadline);
+
+            c.realizar_pago(None, 4000, hoy, precios.clone(), deadline);
+            c.realizar_pago(None, 4000, hoy + 100_000, precios.clone(), deadline);
+
+            d.realizar_pago(None, 5000, hoy, precios.clone(), deadline);
+            d.realizar_pago(None, 5000, hoy + 100_000, precios.clone(), deadline);
+            d.realizar_pago(descuento, 4250, hoy + deadline + 200_000, precios.clone(), deadline);
+
+            f.realizar_pago(None, 2000, hoy, precios.clone(), deadline);
+            f.realizar_pago(None, 2000, hoy + 100_000, precios.clone(), deadline);
+            f.realizar_pago(descuento, 1700, hoy + 200_000, precios.clone(), deadline);
+
+            ink::env::test::set_block_timestamp::<ink::env::DefaultEnvironment>(hoy);
+            let esperado: Vec<Socio> = Vec::new();
+            let gestor = GestorDeCobros::new();
+            let resultado = gestor.socios_morosos();
+
+            assert_eq!(esperado, resultado, "Se esperaba {:#?} y se obtuvo {:#?}", esperado, resultado);
+
+            ink::env::test::set_block_timestamp::<ink::env::DefaultEnvironment>(hoy+deadline*2);
+            let esperado: Vec<Socio> = Vec::from([e.clone(), g.clone()]);
+            let gestor = GestorDeCobros::new();
+            let resultado = gestor.socios_morosos();
+
+            assert_eq!(esperado, resultado, "Se esperaba {:#?} y se obtuvo {:#?}", esperado, resultado);
+
+            ink::env::test::set_block_timestamp::<ink::env::DefaultEnvironment>(hoy+deadline*4);
+            let esperado: Vec<Socio> = Vec::from([b.clone(), c.clone(), e.clone(), g.clone()]);
+            let gestor = GestorDeCobros::new();
+            let resultado = gestor.socios_morosos();
+
+            assert_eq!(esperado, resultado, "Se esperaba {:#?} y se obtuvo {:#?}", esperado, resultado);
+
+            ink::env::test::set_block_timestamp::<ink::env::DefaultEnvironment>(hoy+deadline*5);
+            let esperado: Vec<Socio> = Vec::from([a.clone(), b.clone(), c.clone(), d.clone(), e.clone(), f.clone(), g.clone()]);
+            let gestor = GestorDeCobros::new();
+            let resultado = gestor.socios_morosos();
+
+            assert_eq!(esperado, resultado, "Se esperaba {:#?} y se obtuvo {:#?}", esperado, resultado);
+        }
     }
-    }
+}
 
 
